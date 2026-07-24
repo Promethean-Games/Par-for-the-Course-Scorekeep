a6f2c3f2c9e88700b29dcd09078e28dc635aaf32
Promethean-Games <alan.acevedo@promethean-games.com>
2026-07-24 12:20:46 -0500

    Testing Framework

---
+++ jest.config.cjs
@@ -0,0 +1,8 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  moduleNameMapper: {
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+  },
+  moduleDirectories: ['node_modules', 'src'],
+};
